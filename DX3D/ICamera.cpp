08c20ced7f5cd5b338d34036b3abbcde6366009c
john han <johngamecoder@gmail.com>
2018-05-24 11:19:15 +0900

    Third person camera movement location according to character's movement fixed. So happy and should carry on!

--- DX3D/ICamera.cpp
+++ DX3D/ICamera.cpp
@@ -8,16 +8,13 @@ ICamera::ICamera()
     RECT rc;
     GetClientRect(g_hWnd, &rc);
     m_aspect = rc.right / (float)rc.bottom;
+    m_up = D3DXVECTOR3(0, 1, 0);
 }
 
 void ICamera::Init()
 {
-    m_lookAt = D3DXVECTOR3(0.0f, 0.0f, 1.0f);
-
     m_eye = D3DXVECTOR3(m_basePosX, m_basePosY, -m_distance);
-    UpdateEye();
-
-    m_up = D3DXVECTOR3(0, 1, 0);
+    m_lookAt = D3DXVECTOR3(m_eye.x, m_eye.y, m_eye.z+1);
 
     //m_ptPrevMouse;
     D3DXMatrixLookAtLH(&m_matView, &m_eye, &m_lookAt, &m_up);
@@ -29,18 +26,36 @@ void ICamera::Init()
 
 void ICamera::Update()
 {
+    D3DXMATRIXA16 matWorld;
+    D3DXMatrixIdentity(&matWorld);
+
+    D3DXVECTOR3* pTargetPos = g_pCameraManager->GetTargetPos();
+    D3DXVECTOR3* pTargetRot = g_pCameraManager->GetTargetDir();
+
     m_eye = D3DXVECTOR3(m_basePosX, m_basePosY, -m_distance);
-    UpdateEye();
+    m_lookAt = D3DXVECTOR3(m_eye.x, m_eye.y, m_eye.z + 1);
 
-    //Player가 움직여도 계속 앞을 보게 하려면 1
-    //m_lookAt = D3DXVECTOR3(m_eye.x, m_eye.y-0.1, m_eye.z + 1);
+    D3DXMATRIXA16 matR, matT;
+    D3DXMatrixRotationYawPitchRoll(&matR, pTargetRot->y, pTargetRot->x, pTargetRot->z);
+    
+    D3DXMatrixTranslation(&matT, pTargetPos->x, pTargetPos->y, pTargetPos->z);
+    matWorld = matWorld *matR* matT;
+    D3DXVec3TransformCoord(&m_eye, &m_eye, &matWorld);
+    D3DXVec3TransformCoord(&m_lookAt, &m_lookAt, &matWorld);
+    
+    
+    
+    
+    
 
-    D3DXMATRIXA16 matRot;
-    D3DXMatrixRotationYawPitchRoll(&matRot, m_rotY, m_rotX, 0.0f);
 
-    D3DXVec3TransformCoord(&m_eye, &m_eye, &matRot);
-    //Player가 움직여도 계속 앞을 보게 하려면 2
-    D3DXVec3TransformCoord(&m_lookAt, &m_lookAt, &matRot);
+
+    //D3DXMATRIXA16 matRot;
+    //D3DXMatrixRotationYawPitchRoll(&matRot, m_rotY, m_rotX, 0.0f);
+
+    //D3DXVec3TransformCoord(&m_eye, &m_eye, &matRot);
+    ////Player가 움직여도 계속 앞을 보게 하려면 2
+    //D3DXVec3TransformCoord(&m_lookAt, &m_lookAt, &matRot);
 
     D3DXMatrixLookAtLH(&m_matView, &m_eye, &m_lookAt, &m_up);
     g_pDevice->SetTransform(D3DTS_VIEW, &m_matView);
